ae91011ea43859a713e37bee7943cc2d36593aea
AndrewShadrin <andrew_shadrin@mail.ru>
2019-10-02 22:06:44 +0300

    Сделаны задания 2 урока

---
+++ Lesson-2-1/OrderRepository.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Lesson_2_1
+{
+    class OrderRepository
+    {
+        public Order Load(int orderId)
+        {
+            Order order = new Order();
+            order.Name = "Old order";
+            return order;
+        }
+        public void Save(Order order) { }
+        public void Update(Order order) { }
+        public void Delete(Order order) { }
+    }
+}
---
+++ Lesson-2-2/IItem.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Lesson_2_2
+{
+    interface IItem
+    {
+        void SetDiscount(double discount);
+        void SetPromocode(string promocode);
+        void SetPrice(double price);
+    }
+}
